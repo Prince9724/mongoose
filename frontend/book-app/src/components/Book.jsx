9898d856c5354b7d76207f35ecc957b6d670425f
Prince gond <princegondrw123@gmail.com>
2026-06-30 08:56:03 +0530

    .//.

--- frontend/book-app/src/components/Book.jsx
+++ frontend/book-app/src/components/Book.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { useState } from 'react'
+// import React from 'react'
+// import { useState } from 'react'
 
-const Book = () => {
-    const [books, getbooks]=useState([]);
+// const Book = () => {
+//     const [books, getbooks]=useState([]);
 
-    const fetchbook= async()=>{
-        const res = await axios.get("http://localhost:5000/api/book");
-        getbooks(res.data);
-    }
-  return (
-    <div>Book</div>
-  )
-}
+//     const fetchbook= async()=>{
+//         const res = await axios.get("http://localhost:5000/api/book");
+//         getbooks(res.data);
+//     }
+//   return (
+//     <div>Book</div>
+//   )
+// }
 
-export default Book
+// export default Book
